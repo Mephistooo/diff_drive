897136ff183e7f03e106f2da9d6e1a7e4af1b863
MZ <zaour.majd@gmail.com>
2023-01-22 19:08:20 +0100

    Add launch file

--- src/diff_drive/motor_driver.py
+++ src/diff_drive/motor_driver.py
@@ -18,7 +18,6 @@ class VelocityCommand():
         self.WHEEL_GAP = 0.2
         self.speed = []
 
-        
 
     def map(value: float, from_min: int, from_max: int, to_min: int, to_max: int):
         """
@@ -32,15 +31,6 @@ class VelocityCommand():
         return (value - from_min) * (to_max - to_min) / (from_max - from_min) + to_min
 
     def set_pwm(self, data: Twist):
-        # linear = min(1.0, max(-1.0, data.linear.x))
-        # angular = min(1.0, max(-1.0, data.angular.z))
-
-        # _left_temp = float(((2 * linear) - (angular * self.WHEEL_GAP)) / (2 * self.WHEEL_RADIUS))
-        # _right_temp = float(((2 * linear) + (angular * self.WHEEL_GAP)) / (2 * self.WHEEL_RADIUS))
-        # print(_left_temp, _right_temp)
-
-        # left_temp = ((2 * linear) - (angular * self.WHEEL_GAP)) / (2 * self.WHEEL_RADIUS)
-        # right_temp = ((2 * linear) + (angular * self.WHEEL_GAP)) / (2 * self.WHEEL_RADIUS)
         # #rotation 
         linear = data.linear.x
         angular = data.angular.z
@@ -56,32 +46,16 @@ class VelocityCommand():
         else :
             left_speed = linear - angular * self.WHEEL_GAP / 2.0
             right_speed = linear + angular * self.WHEEL_GAP / 2.0
-            # self._left_speed_percent = (100 * left_speed/self._max_speed)
-            # self._right_speed_percent = (100 * right_speed/self._max_speed)
-            # speed = int(min(max(abs(speed_percent * 255), 0), 255))
 
-        # Calculate wheel speeds in m/s
-        # left_speed = linear - angular*self._wheel_base/2
-        # right_speed = linear + angular*self._wheel_base/2
-
-        # Ideally we'd now use the desired wheel speeds along
-        # with data from wheel speed sensors to come up with the
-        # power we need to apply to the wheels, but we don't have
-        # wheel speed sensors. Instead, we'll simply convert m/s
-        # into percent of maximum wheel speed, which gives us a
-        # duty cycle that we can apply to each motor.
-        _left_speed_percent = (0.01 * left_speed/1.0)
-        _right_speed_percent = (0.01 * right_speed/1.0)
-        speed_percent = float(min(max(abs(_left_speed_percent * 1.0), 0), 1.0))
-        rospy.loginfo('FLE: {0}, FRE: {1}, p_l {2}, p_r {3}, p {4}'.format(left_speed, right_speed, _left_speed_percent, _right_speed_percent, speed_percent))
+        # _left_speed_percent = (0.01 * left_speed/1.0)
+        # _right_speed_percent = (0.01 * right_speed/1.0)
+        # speed_percent = float(min(max(abs(_left_speed_percent * 1.0), 0), 1.0))
+        rospy.loginfo('FLE: {0}, FRE: {1}'.format(left_speed, right_speed))
         self.motor_driver.motor1.throttle = left_speed
         self.motor_driver.motor2.throttle = right_speed
         self.motor_driver.motor3.throttle = left_speed
         self.motor_driver.motor4.throttle = right_speed 
         
-        # rospy.loginfo('FLE: {0}, FRE: {1}, BLE: {2}, BRE:{3}'.format(left_temp, right_temp , round((left_temp / 1000), 2),  round((right_temp / 1000), 2)))
-
- 
     def start_listening(self):
             rospy.Subscriber('/robot/cmd_vel', Twist, self.set_pwm)
             rospy.spin()
