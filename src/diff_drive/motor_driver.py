86babaa468f53f2af2ed42c2a9135301cd6674bd
MZ <zaour.majd@gmail.com>
2023-02-09 14:09:53 +0100

    debug

--- src/diff_drive/motor_driver.py
+++ src/diff_drive/motor_driver.py
@@ -61,8 +61,8 @@ class VelocityCommand():
         # _left_speed_percent = (0.01 * left_speed/1.0)
         #  _right_speed_percent = (0.01 * right_speed/1.0)
 
-        left_speed_percent = float(min(max(abs(left_speed * 1), 0.2), 1.0))
-        right_speed_percent = float(min(max(abs(right_speed * 1), 0.2), 1.0))
+        left_speed_percent = float(min(max(abs(left_speed * 1), 0.4), 1.0))
+        right_speed_percent = float(min(max(abs(right_speed * 1), 0.4), 1.0))
 
         rospy.loginfo('FLE: {}, FRE: {}'.format(left_speed , right_speed))
 
