201c2851814b3e2c2825f7c22464e06b0d834ccb
MZ <zaour.majd@gmail.com>
2023-02-12 11:27:58 +0100

    Fix controller loop

--- src/diff_drive/motor_driver.py
+++ src/diff_drive/motor_driver.py
@@ -9,44 +9,66 @@ import board
 from adafruit_motorkit import MotorKit
 import atexit
 
+class MotorDriver():
+    def __init__(self):
+        self.motor_driver = MotorKit(i2c=board.I2C())
+        atexit.register(self.stop)
+        
+    def _format(speed, minimum, maximum):
+        if speed < minimum:
+            return minimum
+        elif speed > maximum:
+            return maximum
+        return speed
+
+    def set_speed(self, left_speed: float, right_speed: float):
+        self.motor_driver.motor1.throttle = left_speed
+        self.motor_driver.motor2.throttle = right_speed
+        self.motor_driver.motor3.throttle = left_speed
+        self.motor_driver.motor4.throttle = right_speed
+
+    def stop(self):
+        self.motor_driver.motor1.throttle = 0
+        self.motor_driver.motor2.throttle = 0
+        self.motor_driver.motor3.throttle = 0
+        self.motor_driver.motor4.throttle = 0 
+
+
 class VelocityCommand():
 
     def __init__(self):
         self.min = 0.4
         self.max = 1.0
-        self.motor_driver = MotorKit(i2c=board.I2C())
         rospy.init_node('diff_motor_controller')
         self.WHEEL_RADIUS = 0.038
         self.WHEEL_GAP = 0.18
-        atexit.register(self.stop)
         self.left_speed = 0
         self.right_speed = 0 
         self._last_received = rospy.get_time()
         self._timeout = rospy.get_param('~timeout', 2)
         self._rate = rospy.get_param('~rate', 10)
+        self.motor_driver = MotorDriver()
 
-    def map(value: float, from_min: int, from_max: int, to_min: int, to_max: int):
-        """
-            Utility method to nomralize provided values from a range to a specific range of integers provided
-            :param value: float
-            :param from_min: int
-            :param from_max: int
-            :param to_min: int
-            :param to_max: int    
-        """
-        return (value - from_min) * (to_max - to_min) / (from_max - from_min) + to_min
+    def run(self):
+            """driver loop"""
+            rate = rospy.Rate(self._rate)
 
+            while not rospy.is_shutdown():
+                delay = rospy.get_time() - self._last_received
+                if delay < self._timeout:
+                    self.motor_driver.set_speed(self.left_speed, right_speed)
+                else:
+                    self.motor_driver.Stop()
+
+                rate.sleep()
 
     def set_pwm(self, data: Twist): 
-        rate = rospy.Rate(10)
         self._last_received = rospy.get_time()
         linear = data.linear.x
         angular = data.angular.z
         if angular == 0 and linear == 0:
-            self.motor_driver.motor1.throttle = 0
-            self.motor_driver.motor2.throttle = 0
-            self.motor_driver.motor3.throttle = 0
-            self.motor_driver.motor4.throttle = 0
+            self.left_speed= = 0
+            self.right_speed = 0
             return
         elif linear == 0 :
             right_speed = angular * self.WHEEL_GAP / 2.0
@@ -56,6 +78,11 @@ class VelocityCommand():
         else :
             left_speed = linear - angular * self.WHEEL_GAP / 2.0
             right_speed = linear + angular * self.WHEEL_GAP / 2.0
+
+        if left_speed < 0.1:
+            left_speed = 0 
+        if right_speed < 0.01:
+            right_speed = 0 
 
         # prece = int(speed_percent / 255 * 100)
         # speed = int(min(max(abs(speed_percent * 255), 0), 255))
@@ -68,42 +95,21 @@ class VelocityCommand():
 
         self.left_speed= -left_speed_percent if left_speed < 0 else left_speed_percent
         self.right_speed = -right_speed_percent if right_speed < 0 else right_speed_percent
-        self.motor_driver.motor1.throttle = self.left_speed
-        self.motor_driver.motor2.throttle = self.right_speed
-        self.motor_driver.motor3.throttle = self.left_speed
-        self.motor_driver.motor4.throttle = self.right_speed
-
-        return
-
-    def stopAll(self):
-        self.motor_driver.motor1.throttle = 0
-        self.motor_driver.motor2.throttle = 0
-        self.motor_driver.motor3.throttle = 0
-        self.motor_driver.motor4.throttle = 0 
-    
-    def set_speed(self):
-        self.motor_driver.motor1.throttle = self.left_speed
-        self.motor_driver.motor2.throttle = self.right_speed
-        self.motor_driver.motor3.throttle = self.left_speed
-        self.motor_driver.motor4.throttle = self.right_speed
+        # self.motor_driver.motor1.throttle = self.left_speed
+        # self.motor_driver.motor2.throttle = self.right_speed
+        # self.motor_driver.motor3.throttle = self.left_speed
+        # self.motor_driver.motor4.throttle = self.right_speed
 
     def start_listening(self):
             rospy.Subscriber('/cmd_vel', Twist, self.set_pwm)
             rospy.spin()
-            
-    def stop(self):
-        """Stop all movement."""
-        self.motor_driver.motor1.throttle = 0
-        self.motor_driver.motor2.throttle = 0
-        self.motor_driver.motor3.throttle = 0
-        self.motor_driver.motor4.throttle = 0 
 
 if __name__ == '__main__':
     velocity_command = VelocityCommand()
     # velocity_command.start_listening()
     try :
-        # _thread.start_new_thread(velocity_command.set_pwm, ())
         velocity_command.start_listening()
+        _thread.start_new_thread(velocity_command.run, ())
     except rospy.ROSInterruptException:
         velocity_command.stopAll()
         pass
