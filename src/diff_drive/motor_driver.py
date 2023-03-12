2a34b134234116843547f59e8dde8028f198c4c5
MZ <zaour.majd@gmail.com>
2023-03-12 13:29:31 +0100

    WIP

--- src/diff_drive/motor_driver.py
+++ src/diff_drive/motor_driver.py
@@ -129,13 +129,13 @@ class VelocityCommand():
         # rospy.spin()
         rate = rospy.Rate(self._rate)
         while not rospy.is_shutdown():
-            self.set_speed(self.left_speed, self.right_speed)
-            # delay = rospy.get_time() - self._last_received
-            # if delay < self._timeout:
-                # self.set_speed(self.left_speed, self.right_speed)
-            # else:
-                # self.stop()
-            # rate.sleep()
+            # self.set_speed(self.left_speed, self.right_speed)
+            delay = rospy.get_time() - self._last_received
+            if delay < self._timeout:
+                self.set_speed(self.left_speed, self.right_speed)
+            else:
+                self.stop()
+            rate.sleep()
 
 if __name__ == '__main__':
     velocity_command = VelocityCommand()
